71bb587eb066b2449d580a61d742fa008eee4c81
Michal Špaček <mail@michalspacek.cz>
2024-06-01 20:17:38 +0200

    Script to update local GitLeaks

---
+++ bin/update-gitleaks.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+DIR=~/.local/share/gitleaks/
+
+if ! hash gh 2>/dev/null; then
+    echo "gh is required but it's not installed, see https://cli.github.com/"
+    exit 2
+fi
+
+REPO=gitleaks/gitleaks
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+LATEST="$("$SCRIPT_DIR"/github-latest-tag.sh "$REPO")"
+ARCHIVE="gitleaks_${LATEST}_linux_x64.tar.gz"
+
+if [[ $DIR != ~/.local/share/* ]]; then
+    echo "Oops, $DIR is not a safe dir to extract GitLeaks to"
+    exit 1
+fi
+
+cd $DIR || exit 3
+
+echo "Purging $DIR"
+rm -rf "${DIR:?}/*"
+
+echo "Downloading $REPO $LATEST to $DIR"
+gh release download --repo "$REPO" "v$LATEST" --pattern "$ARCHIVE"
+
+echo "Extracting $ARCHIVE"
+tar xvf "$ARCHIVE" > /dev/null
+
+echo "Deleting $ARCHIVE"
+rm "$ARCHIVE"
+
+echo "Done"
+cd - || exit 4
